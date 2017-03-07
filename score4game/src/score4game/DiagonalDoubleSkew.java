fbd82b7ef227c2d75a2a41b0f008d2365bad0028
Mitchell Read <fox_mitchell1@hotmail.com>
2017-03-06 21:54:26 -0800

    test

--- score4game/src/score4game/DiagonalDoubleSkew.java
+++ score4game/src/score4game/DiagonalDoubleSkew.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package score4game;
 
-/**
- *
- * @author Shahana Ala
- * 
- */
 //Tested
 public class DiagonalDoubleSkew extends Lines{
 
