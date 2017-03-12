9fb1318cef1113c54b3d2506797ee9aaad318021
ShahanaIrene <alam@unbc.ca>
2017-03-11 20:04:40 -0800

    Line array

--- score4game/src/score4game/Board.java
+++ score4game/src/score4game/Board.java
@@ -3,6 +3,7 @@ package score4game;
 public class Board {
     private final int myLength = 4;
     private final Peg [][] myGrid;
+    private Lines[] myLines;
     
     public Board (){
         myGrid = new Peg [myLength][myLength]; //create a 4*4 board grid
@@ -15,6 +16,9 @@ public class Board {
         for (int i = 0; i < myLength; i++)
             for (int j = 0; j < myLength; j++)
                myGrid[i][j].addLocation(i, j);
+        
+        myLines = new Lines[]{new HorizontalLines(), new VerticalLines(), new StraightDiagonalLines(),
+                    new DiagonalSingleSkew(), new DiagonalDoubleSkew()};
     }
 
     //action listeners for player adding bead with click?
@@ -25,6 +29,22 @@ public class Board {
                     myGrid[i][j].addBead(color);
                     return;
                 }
+    }
+    
+    public boolean isWhiteWin(){
+        for (Lines thisLine:myLines){
+            if (thisLine.isWhiteLine(this))
+                return true;
+        }
+        return false;
+    }
+    
+    public boolean isBlackWin(){
+        for (Lines thisLine:myLines){
+            if (thisLine.isBlackLine(this))
+                return true;
+        }
+        return false;
     }
 
     public int getMyLength(){
@@ -37,6 +57,22 @@ public class Board {
 
     public Peg getPeg(int row, int column){
        return myGrid[row][column];
+    }
+    
+    public void resetBoard(){
+        for (int i = 0; i < myLength; i++){
+           for (int j = 0; j < myLength; j++)
+               myGrid[i][j].resetPeg();
+        }
+    }
+    
+    public void removeBead(Location location){
+        for (int i = 0; i < myLength; i++)
+            for (int j = 0; j < myLength; j++)
+                if (myGrid[i][j].getLocation().equals(location)){
+                    myGrid[i][j].removeBead();
+                    return;
+                } 
     }
 
     //temporary don't need when GUI is working
