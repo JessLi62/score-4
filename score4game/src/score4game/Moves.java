e9b65f1521219603255d1db049b88f2e9884b6e6
Mitchell Read <fox_mitchell1@hotmail.com>
2017-03-12 12:09:23 -0700

    ColourChooser class is the 2nd JFrame window added methods to Referee Moves class added

--- score4game/src/score4game/Moves.java
+++ score4game/src/score4game/Moves.java
@@ -1,79 +1,90 @@
 package score4game;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import static score4game.Colour.BLACK;
+import static score4game.Colour.WHITE;
 
 public class Moves {
+
     private ArrayList<Location> listOfMoves = new ArrayList();
-    
+
     private int numberOfMoves;//?irrelevant?
-    
-    public Moves(){
-        
-        char [] rowChar = {'a','b','c','d'};
-        for (int i=0; i < 4; i++)
-            for (int j = 0; j< 4; j++){
-            listOfMoves.add(new Location(i,j));
+
+    public Moves() {
+
+        char[] rowChar = {'a', 'b', 'c', 'd'};
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                listOfMoves.add(new Location(i, j));
+            }
         }
     }
-    
-    public int getMovesMade(){
+
+    public int getMovesMade() {
         return numberOfMoves;
     }
-    
-    public Location getMove(Board gameBoard, Color playerColor){
-        
+
+    public Location getMove(Board gameBoard, Color playerColor) {
+
         Boolean moveFound = false;
         Location moveToMake;
-        
-        while (!moveFound){
-           
+
+        while (!moveFound) {
+
             gameBoard.displayBoard();
-            
-            for (int i = 0; i< listOfMoves.size(); i++){
+
+            for (int i = 0; i < listOfMoves.size(); i++) {
                 moveToMake = listOfMoves.get(i);
-                if (!(gameBoard.getPeg(moveToMake).isFull()))
+                if (!(gameBoard.getPeg(moveToMake).isFull())) {
                     gameBoard.addBead(moveToMake, playerColor);
-                else
+                } else {
                     continue;
-                
-                if (isLineComplete(gameBoard, playerColor))
+                }
+
+                if (isLineComplete(gameBoard, playerColor)) {
                     return moveToMake;
-                
+                }
+
                 gameBoard.removeBead(moveToMake);
             }
-            
-            for (int i = 0; i< listOfMoves.size(); i++){   
+
+            for (int i = 0; i < listOfMoves.size(); i++) {
                 moveToMake = listOfMoves.get(i);
-                
-                if (!(gameBoard.getPeg(moveToMake).isFull()))
+
+                if (!(gameBoard.getPeg(moveToMake).isFull())) {
                     gameBoard.addBead(moveToMake, playerColor.getOppositeColor());
-                else
+                } else {
                     continue;
-                
-                if (isLineComplete(gameBoard, playerColor.getOppositeColor())){
+                }
+
+                if (isLineComplete(gameBoard, playerColor.getOppositeColor())) {
                     gameBoard.removeBead(moveToMake);
                     gameBoard.addBead(moveToMake, playerColor);
-                    return moveToMake;}
-                
+                    return moveToMake;
+                }
+
                 gameBoard.removeBead(moveToMake);
             }
-            
-            for (int i=0; i < listOfMoves.size(); i++){
-                if ((gameBoard.getPeg(listOfMoves.get(i))).isFull()== false){
+
+            for (int i = 0; i < listOfMoves.size(); i++) {
+                if ((gameBoard.getPeg(listOfMoves.get(i))).isFull() == false) {
                     gameBoard.addBead(listOfMoves.get(i), playerColor);
                     return listOfMoves.get(i);
-                }}
-            
+                }
+            }
+
         }
         //Possibly throw exception
         return listOfMoves.get(0);
     }
-    
-    
-    private boolean isLineComplete(Board gameBoard, Color color){
-        switch (color){
-            case BLACK: return gameBoard.isBlackWin();
-            case WHITE: return gameBoard.isWhiteWin();
+
+    private boolean isLineComplete(Board gameBoard, Color color) {
+        switch (color) {
+            case BLACK:
+                return gameBoard.isBlackWin();
+            case WHITE:
+                return gameBoard.isWhiteWin();
         }
         return false; // throw exception?
     }
